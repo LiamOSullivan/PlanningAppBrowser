48b8dd93d817870832a3a448b267f84165fc5e31
LiamOSullivan <lmosulli@tcd.ie>
2019-08-28 10:55:48 +0100

    First commit of demo runnning locally

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const path = require('path');
+const uttil = require('util');
+const express = require('express');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.listen(3000, () => console.log("Server listening"));
+
+module.exports = app;
