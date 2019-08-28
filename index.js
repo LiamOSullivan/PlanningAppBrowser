423ee38225a7478d8b91a63feab8c92a90f81f65
LiamOSullivan <lmosulli@tcd.ie>
2019-08-28 14:31:28 +0100

    Refactor filenames

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 const path = require('path');
 const express = require('express');
 
@@ -6,6 +6,5 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
-// app.listen(3000, () => console.log("Server listening"));
 
 module.exports = app;
